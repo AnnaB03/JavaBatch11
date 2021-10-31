f46aab8cb854ee1600887dab75c8535ca7bacc1c
anna <annabugrinskaya@gmail.com>
2021-10-31 18:31:09 -0400

    adding new

--- src/com/syntax/class08/HWClock.java
+++ src/com/syntax/class08/HWClock.java
@@ -5,10 +5,12 @@ public class HWClock {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		for (double i=0.00; i<=11.59; i++) { 
+		for (double hours=0; hours<=12; hours++) { 
 			
-			for (double j=0.00; j<=0.59; j++) { 
-				System.out.println(i+" "+j);
+			for (double minutes=0; minutes<=59; minutes++) { 
+				
+					
+				System.out.println(hours+":"+minutes);
 				
 			}
 		} System.out.println("The end of the code");
